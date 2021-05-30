fa31788057bc7cf310fce8acc606c73b49974e21
abdulrehman ajmal <abdulrehman.ajmal@outlook.com>
2021-05-31 00:01:30 +0500

    human detection

--- humanDetection.py
+++ humanDetection.py
@@ -9,8 +9,8 @@ while (True):
     gray = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
     # cascade classifier
     
-    human = cascade.detectMultiScale(gray , 1.5,minNeighbors = 3)
-    
+    human = cascade.detectMultiScale(gray , 1.5,minNeighbors = 1)
+    # if we select less neighbours we can have  more detections
     for (x,y,z,w) in human:
         cv.rectangle(image,(x,y),(x+z,y+w),(200,220,140),thickness=2)
         cv.imshow('human', image)
